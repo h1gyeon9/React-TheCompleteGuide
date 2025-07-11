e86155810207cb17d4f51eb95f60345559f82d9e
Huigyeong Na <nhk1007@naver.com>
2025-07-11 11:40:35 +0900

    Day 4 - section 2 Component 분리 / Wrapped Element에 Props 전달 / Component Type 동적 설정

--- section2/src/App.jsx
+++ section2/src/App.jsx
@@ -1,55 +1,17 @@
-import {useState} from 'react';
-
-import componentsImg from './assets/components.png';
-import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TapButton.jsx';
-import {EXAMPLES} from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('');
-
-  function handleSelect(selectedButton){
-    setSelectedTopic(selectedButton);
-    //console.log(selectedTopic); 이전 값 표시됨.
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic){
-    tabContent = (
-    <div id="tab-content">
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>{EXAMPLES[selectedTopic].code}</code>
-      </pre>
-    </div>
-  )}
 
   return (
-    <div>
+    <>  {/*JSX는 하나의 값을 반환함. 이하는 하나로 묶는 세 가지 방식 1. div가 없으면 Header와 main 두 개의 값이 됨. div로 묶어서 하나로 만들기. 2. Fragment를 사용하면 불필요한 div 없이 하나로 묶을 수 있음. 3. 빈 태그를 사용함 */}
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem} />))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-            {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ section2/src/components/Tabs.jsx
@@ -0,0 +1,9 @@
+import { Children } from "react";
+
+export default function Tabs({children, buttons, buttonsContainer='menu'}) {
+    const ButtonContainer = buttonsContainer;
+    return <>
+        <ButtonContainer>{buttons}</ButtonContainer>
+        {children}
+    </>
+}
